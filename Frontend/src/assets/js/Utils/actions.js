b3be1f3ca29132bd5a2723d07ded4d57700358b8
Willian <williandeivitidaniel@live.com>
2024-04-29 00:34:45 -0300

    make ajustments on actions

--- Frontend/src/assets/js/Utils/actions.js
+++ Frontend/src/assets/js/Utils/actions.js
@@ -1,5 +1,5 @@
-export const signUpAction = 'https://freedevtools.free.nf//api/signup.php'
+export const signUpAction = 'https://freedevtools.free.nf/api/signup.php'
 
-export const signInAction = 'https://freedevtools.free.nf//api/signin.php'
+export const signInAction = 'https://freedevtools.free.nf/api/signin.php'
 
-export const confirmCodeAction = 'https://freedevtools.free.nf//api/confirm-account.php'
+export const confirmCodeAction = 'https://freedevtools.free.nf/api/confirm-account.php'
